e6c2bfd39a3f884f1bb1b1a603ac291750453b9e
FabioLopes021 <a25999@alunos.ipca.pt>
2023-01-08 15:49:48 +0000

    Structurs

---
+++ ProjetoPI/main.c
@@ -0,0 +1,19 @@
+// incluir Bibliotecas
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include "Source.h"
+#include "Source.c"
+
+
+
+
+
+int main() {
+	pratData c[MAX_PART];
+	trainingType p[MAX_PART],  z[MAX_PART];
+	trainingPlan t[MAX_PART], a[MAX_PART];
+
+
+	return 0;
+}
